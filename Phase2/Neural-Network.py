af686b1758cc00ba52bc4f579ad0110ad846733a
AmirHosseinTaghavi <34903341+AmirHosseinTaghavi@users.noreply.github.com>
2018-01-30 06:03:35 +0330

    Update Neural-Network.py

--- Phase2/Neural-Network.py
+++ Phase2/Neural-Network.py
@@ -6,7 +6,7 @@ import neurolab as nl
 import re as re
 
 def get_manipulated_train():
-	df = pd.read_csv('./data/train.csv',header=0)
+	df = pd.read_csv('../data/train.csv',header=0)
 	columns = ['Ticket','Cabin']
 	df = df.drop(columns,axis=1)
 	redundant = []
